acdacecf249671ec71c6a4c2a51103d6547050bb
Agustin Monetti <agmonetti@uade.edu.ar>
2026-01-03 13:36:54 -0300

    new project structure

--- PilaEstatica/interfaz/pilaTDA.java
+++ PilaEstatica/interfaz/pilaTDA.java
@@ -1,9 +1,10 @@
-package PilaEstatica;
+package interfaz;
 
 public interface pilaTDA {
     void InicializarPila();     // Sin precondiciones. Crea pila vacia
     void Apilar(int x);         // Pila inicializada. Agrega un numero a la pila, acordarse de que lo va agregando arriba y solo ten
     void Desapilar();           // Pila inicializada y no vacía. Elimina el de arriba del todo, el que esta en el tope
-    int Tope();                 // Pila inicializada y no vacía.Me muestra el numero que esta arriba del todo.Debo asegurarme que la pila no este vacia
+    int Tope();                 // Pila inicializada y no vacía.Me muestra el numero que esta arriba del todo.
+    							// Debo asegurarme que la pila no este vacia
     boolean PilaVacia();        // Pila inicializada. Me dice si la pila esta vacia o no
 }
---
+++ PilaEstatica/principal/princ.java
@@ -0,0 +1,14 @@
+package principal;
+
+import implementacion.pilaTF; 
+import interfaz.pilaTDA;
+
+
+public class princ {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
