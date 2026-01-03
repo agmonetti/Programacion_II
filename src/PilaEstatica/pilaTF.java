340320c65ea166a6fe37e4c665b6112e4b9a20f5
Agustin Monetti <agmonetti@uade.edu.ar>
2026-01-03 13:12:26 -0300

    pila static createdd

---
+++ src/PilaEstatica/pilaTF.java
@@ -0,0 +1,35 @@
+package PilaEstatica;
+
+public class pilaTF implements pilaTDA {
+
+	@Override
+	public void InicializarPila() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Apilar(int x) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Desapilar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int Tope() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean PilaVacia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
