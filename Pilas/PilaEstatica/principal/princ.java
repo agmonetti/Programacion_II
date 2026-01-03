6c9b075a0324c3945590a5d1b45c5d0a36fb38f8
Agustin Monetti <agmonetti@uade.edu.ar>
2026-01-03 14:35:28 -0300

    copiar pila implemented - now we can modify the mostrarPila method to not destroy the pila

--- Pilas/PilaEstatica/principal/princ.java
+++ Pilas/PilaEstatica/principal/princ.java
@@ -15,6 +15,35 @@ public class princ {
 		}
 		
 	}
+	
+	public static pilaTDA copiarPila(pilaTDA pilaOriginal) {
+		pilaTDA pilaAux = new pilaTF();
+		pilaTDA pilaCopia = new pilaTF();
+		
+		pilaAux.InicializarPila();
+		pilaCopia.InicializarPila();
+		
+		int x;
+		
+		while(!pilaOriginal.PilaVacia()) {
+			x = pilaOriginal.Tope();
+			pilaAux.Apilar(x);
+			pilaOriginal.Desapilar();
+		}
+		
+		while(!pilaAux.PilaVacia()) {
+			x = pilaAux.Tope();
+			pilaCopia.Apilar(x);
+			pilaOriginal.Apilar(x);       //TENGO QUE APILAR DE NUEVO A LA ORIGINAL PORQUE SINO ROMPO EL PARAMETRO QUE LE PASE
+			pilaAux.Desapilar();
+		}
+		
+		
+		return pilaCopia;
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
 		//hola
@@ -24,8 +53,17 @@ public class princ {
 		pila.Apilar(2);
 		pila.Apilar(3);
 		pila.Apilar(4);
-		
+		System.out.println("Procedemos a copiar la pila");
+		pilaTDA copiaPila = copiarPila(pila);
+		//copio antes de mostrar, porque el metodo de mostrar la pila la borra.
+		System.out.println("pila1: ");
+		System.out.println();
 		mostrarPila(pila);
+		System.out.println();
+		System.out.println("pila2: ");
+
+		mostrarPila(copiaPila);
+		
 	}
 
 }
