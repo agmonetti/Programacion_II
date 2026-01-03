54318eb38b4595cac13512ba09cce74cb40b9579
Agustin Monetti <agmonetti@uade.edu.ar>
2026-01-03 14:22:43 -0300

    static working

--- Pilas/PilaEstatica/principal/princ.java
+++ Pilas/PilaEstatica/principal/princ.java
@@ -5,10 +5,27 @@ import interfaz.pilaTDA;
 
 
 public class princ {
+	
+	public static void mostrarPila(pilaTDA pila)  {
+		int valor;
+		while(!pila.PilaVacia()) {
+			valor = pila.Tope();
+			System.out.println("valor: " + valor);
+			pila.Desapilar();
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		//hola
-
+		pilaTDA pila = new pilaTF();
+		pila.InicializarPila();
+		pila.Apilar(1);
+		pila.Apilar(2);
+		pila.Apilar(3);
+		pila.Apilar(4);
+		
+		mostrarPila(pila);
 	}
 
 }
